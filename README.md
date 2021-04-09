891bc2e76270a14484561b67040daef0ca7a9d1d
Nathan Dunn <me@ndunn.uk>
2021-04-09 12:29:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Laravel Model Respositories
+Wraps around eloquent models allowing them to be used as repositories. 
+
+## Why?
+Because getting a collection from a model "object" isn't really a models
+role, and is deceptive when injecting a model and using it in another context
+
+Inspired by [Jack Wagstaffe](https://github.com/jackowagstaffe)
---
+++ src/Repository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace PackagedBy\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+/**
+ * Abstract Repository
+ * @package PackagedBy\Repositories
+ */
+abstract class Repository
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @param $method
+     * @param $args
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        $helpers = [
+            'getPaginated' => ['paginate'],
+            'getCount' => ['count'],
+            'getSortedAndPaginated' => ['sortable', 'paginate'],
+            'getSorted' => ['sortable'],
+            'get' => ['get'],
+            'firstOrFail' => ['firstOrFail'],
+            'first' => ['first'],
+        ];
+
+        foreach ($helpers as $helper => $helperMethods) {
+            if (Str::startsWith($method, $helper)) {
+                $restOfMethod = ucfirst(Str::replaceFirst($helper, '', $method));
+
+                if (method_exists($this, $restOfMethod)) {
+                    $helperMethods = collect($helperMethods);
+
+                    return $helperMethods->reduce(function ($carry, $item) use ($args) {
+                        return $carry->$item();
+                    }, call_user_func_array([$this, $restOfMethod], $args));
+                }
+            }
+        }
+
+        return call_user_func_array([$this->model, $method], $args);
+    }
+}
